d34a61e8d897bf966707a772c6e46fd7f783c399
danilin52 <pipa-pipkin04@mail.ru>
2025-12-19 13:22:52 +0300

    Update SuperUser.php

--- lab7/MyProject/Classes/SuperUser.php
+++ lab7/MyProject/Classes/SuperUser.php
@@ -2,6 +2,18 @@
 declare(strict_types=1);
 namespace MyProject\Classes;
 require_once 'User.php';
+/**
+ * Класс супер-пользователя с расширенными правами
+ * 
+ * Наследует функциональность базового класса User и добавляет
+ * дополнительные возможности для пользователей с административными правами.
+ * 
+ * @package MyProject\Classes
+ * @extends User
+ * @author Ваше Имя
+ * @version 1.1.0
+ * @since 2024
+ */
 class SuperUser extends User
 {
     public $role;
