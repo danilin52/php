5d0a0216107cf26835d242abdc731c011b808878
danilin52 <pipa-pipkin04@mail.ru>
2025-12-24 14:46:16 +0300

    Update map.php

--- lab2/map.php
+++ lab2/map.php
@@ -1,5 +1,15 @@
 <?php
-function map($array, $callback) {
+declare(strict_types=1);
+
+/**
+ * Применяет callback-функцию ко всем элементам массива
+ * 
+ * @param array $array Исходный массив
+ * @param callable $callback Функция, применяемая к каждому элементу
+ * @return array Новый массив с результатами применения callback-функции
+ */
+ 
+function map(array $array, callable $callback): array {
     $res = [];
     foreach ($array as $element) {
         $res[] = $callback($element);
